7e9cafd6807b410dee759230dfc8104076286d5a
dom <iam@bapairaew.com>
2024-08-28 15:18:07 +0700

    loading indicator

--- app.vue
+++ app.vue
@@ -1,4 +1,8 @@
 <template>
+  <NuxtLoadingIndicator
+    :color="false"
+    class="fixed top-0 z-50 h-1 shadow-lg bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
+  />
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
